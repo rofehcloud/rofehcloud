0919abaed783e2d721feffd2e56dc8acf8df8293
Victor Gartvich <victor.gartvich@gmail.com>
2024-11-19 11:21:56 -0800

    Bug fix for LLM validation step; polish the PIP package

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "": ["media/*.png"],
+    },
+)
